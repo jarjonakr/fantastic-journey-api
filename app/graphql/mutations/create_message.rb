8d2dffbc5092556eaf42f9c16d1ec694eb150506
jose arjona <jarjona@kaufmanrossin.com>
2021-08-29 22:17:52 -0400

    fja04 now is subscribed and we can see on another app

--- app/graphql/mutations/create_message.rb
+++ app/graphql/mutations/create_message.rb
@@ -9,12 +9,8 @@ module Mutations
   
         begin
           message = Message.create!(message_params).tap do |message|
-            puts 'checking chat room id', message_params.inspect, message_params[:chat_room_id]
-            thing = FantasticJourneyApiSchema.subscriptions.trigger(:message_added_to_room, {room_id: message_params[:chat_room_id]}, message)
-            puts 'THING', thing
-            thing
+            FantasticJourneyApiSchema.subscriptions.trigger(:message_added_to_room, {chat_room_id: message_params[:chat_room_id]}, message)
           end
-          puts 'MESSAGE', message
           { message: message }
         rescue ActiveRecord::RecordInvalid => e
           GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
